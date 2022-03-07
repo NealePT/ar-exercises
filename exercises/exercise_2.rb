b1656641c0bf5de06f4bbc82a1e0999783b44210
Neale Taylor <nealeptaylor@gmail.com>
2022-03-07 14:53:31 -0500

    implement exercise 2 logic

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,15 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+# puts Store.find_by(id: 1)
+#Setting store 1 on a variable 
+@store1 = Store.find_by(id: 1)
+puts @store1
+puts @store1.name
+#Setting store 2 on a variable
+@store2 = Store.find_by(id: 2)
+puts @store2
+puts @store2.name
+#Updating store1
+@store1.name = "BurnaBYE"
+puts @store1.name
