7f6b1c3a20fca59e772aa6398f652e4f0ad73e20
Neale Taylor <nealeptaylor@gmail.com>
2022-03-07 15:38:19 -0500

    implement exercise 6 logic

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Neale", last_name: "Taylor", hourly_rate: 80)
+@store1.employees.create(first_name: "Jeff", last_name: "Goldblum", hourly_rate: 500)
+@store2.employees.create(first_name: "John", last_name: "Stamos", hourly_rate: 50)
+@store2.employees.create(first_name: "Afie", last_name: "Jurvanen", hourly_rate: 80)
+@store2.employees.create(first_name: "Jim", last_name: "Halpert", hourly_rate: 30)
